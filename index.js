b41729e30147ff0ba8824a8917d0d676ba772893
comvn <145321168+comvn@users.noreply.github.com>
2025-12-04 10:49:08 +0000

    1

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+
+  const userAgent = req.headers['user-agent'] || '';
+
+  if (userAgent.toLowerCase().includes('cron-job.org')) {
+    console.log('cron-job.org is working! Api alive.');
+  }
+  else {
+    const logData = {
+      method: req.method,
+      url: req.originalUrl,
+      headers: req.headers,
+      body: req.body,
+    };
+
+    console.log(JSON.stringify(logData, null, 2));
+  }
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Get OK" });
+});
+
+app.post("/", (req, res) => {
+  res.json({ message: "Post OK" });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
